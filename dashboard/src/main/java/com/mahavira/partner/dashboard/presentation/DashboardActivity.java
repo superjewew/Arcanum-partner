f11346652ab8c5e6745438711325d67a245835f7
Norman Lie <norman.lie@kudo.co.id>
2018-07-23 17:54:48 +0700

    implement method for getting partner profile

--- dashboard/src/main/java/com/mahavira/partner/dashboard/presentation/DashboardActivity.java
+++ dashboard/src/main/java/com/mahavira/partner/dashboard/presentation/DashboardActivity.java
@@ -1,12 +1,19 @@
 package com.mahavira.partner.dashboard.presentation;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
 
 import com.mahavira.partner.base.presentation.BaseActivity;
+import com.mahavira.partner.base.presentation.ExtraInjectable;
 import com.mahavira.partner.dashboard.R;
 import com.mahavira.partner.dashboard.databinding.ActivityDashboardBinding;
 
-public class DashboardActivity extends BaseActivity<ActivityDashboardBinding, DashboardViewModel> {
+public class DashboardActivity extends BaseActivity<ActivityDashboardBinding, DashboardViewModel> implements ExtraInjectable {
+
+    public static final String EMAIL_EXTRA = "email";
+
+    private String mPartnerEmail;
 
     @Override
     public int getViewModelBindingVariable() {
@@ -21,6 +28,27 @@ public class DashboardActivity extends BaseActivity<ActivityDashboardBinding, Da
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+
+        getViewModel().getPartnerData().observe(this, partner -> {
+            if(partner != null) {
+                switch (partner.status) {
+                    case SUCCESS:
+                        Toast.makeText(this, "Load Profile Success", Toast.LENGTH_SHORT).show();
+                        break;
+                    case ERROR:
+                        Toast.makeText(this, "Load Profile Failed, " + partner.message, Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+
+        getViewModel().attemptGetProfile(mPartnerEmail);
+    }
+
+    @Override
+    public void injectExtras(@NonNull Bundle extras) {
+        if(extras.containsKey(EMAIL_EXTRA)) {
+            mPartnerEmail = extras.getString(EMAIL_EXTRA);
+        }
     }
 }
