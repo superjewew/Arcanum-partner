5555418f29a86d1a417a810a8521e7bb5dbe5653
Norman Lie <norman.lie@kudo.co.id>
2018-07-23 13:25:39 +0700

    create basic dashboard screen

---
+++ dashboard/src/main/java/com/mahavira/partner/dashboard/presentation/DashboardRouter.java
@@ -0,0 +1,12 @@
+package com.mahavira.partner.dashboard.presentation;
+
+import android.content.Context;
+
+/**
+ * Created by norman on 23/07/18.
+ *
+ */
+
+public interface DashboardRouter {
+    void goToDashboard(Context context);
+}
